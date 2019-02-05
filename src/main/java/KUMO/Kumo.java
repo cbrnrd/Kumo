9516d6f7f76177e0bde94fb1d6021da6f01b0d6b
cbrnrd <cbawsome77@gmail.com>
2019-02-05 18:39:35 -0500

    Finish licensing/first run

--- src/main/java/KUMO/Kumo.java
+++ src/main/java/KUMO/Kumo.java
@@ -35,51 +35,7 @@ public class Kumo extends Application {
     }
 
     public static void main(String[] args) {
-
-        File firstStartUp = new File(System.getProperty("java.io.tmpdir") + ".kumoStartup");
-        if (firstStartUp.exists()){
-            // Not first run, check if key is activated
-            try {
-                String key = new Scanner(firstStartUp).useDelimiter("\\A").next();
-                String st = new Scanner(new URL("http://45.55.208.158:8001/id-info/" + key).openStream(), "UTF-8").useDelimiter("\\A").next();
-                if (st.equals("{\"exists\":true,\"activated\":true}")){
-                    // continue to launch normally
-                } else {
-                    // Key doesn't exists or hasn't been activated
-                }
-            } catch (IOException ioe){
-                new AlertView().showErrorAlertWait("Unable to verify product key. Please check your internet connection");
-                System.exit(1);
-            }
-        } else {
-            // This is the first run, have user enter key and check it with the server
-            Stage stage = new Stage();
-            stage.setMinHeight(300);
-            stage.setMinWidth(300);
-            stage.initStyle(StageStyle.DECORATED);
-            stage.setScene(new Scene(new FirstRunView().getFirstRunView(stage), 300, 225));
-            stage.setOnCloseRequest(a -> System.exit(1)); // If user exits this window, close everything and dont write it
-            FirstRunView.getSend().setOnAction(event -> {
-                try {
-                    String st = new Scanner(new URL("http://45.55.208.158:8001/check-id/" + FirstRunView.getTextField().getText().trim()).openStream(), "UTF-8").useDelimiter("\\A").next();
-                    if (st.equals("{\"status\":\"false\"}")) {
-                        new AlertView().showErrorAlertWait("This software was either pirated or downloaded illegally. Please buy it at https://selly.gg/p/b7e477ee.");
-                        System.exit(1);
-                    } else if (st.equals("{\"status\":\"true\"}")){
-                        new URL("http://45.55.208.158/activate-id/" + FirstRunView.getTextField().getText().trim()).openStream(); // No return body
-                        // Write key to file
-
-                    }
-                } catch (IOException ioe){
-                    new AlertView().showErrorAlertWait("\"Unable to verify ID. Please check your internet connection.\"");
-                    System.exit(1);
-                }
-            });
-            stage.showAndWait();
-        }
-
         if (lockInstance()) {
-            //SvgImageLoaderFactory.install(new PrimitiveDimensionProvider());
             launch(args);
         } else {
             System.exit(0);
@@ -120,6 +76,59 @@ public class Kumo extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException, ClassNotFoundException {
         KUMO.Kumo.primaryStage = primaryStage;
+
+        File firstStartUp = new File(System.getProperty("java.io.tmpdir") + ".kumoStartup");
+        if (firstStartUp.exists()){
+            System.out.println("Checking validity of key");
+            // Not first run, check if key is activated
+            try {
+                String key = new Scanner(firstStartUp).useDelimiter("\\A").next();
+                String st = new Scanner(new URL("http://45.55.208.158:8001/id-info/" + key).openStream(), "UTF-8").useDelimiter("\\A").next();
+                if (st.equals("{\"exists\":true,\"activated\":true}")){
+                    // continue to launch normally
+                } else {
+                    new AlertView().showErrorAlertWait("Product key does not exist. Please email KumoRAT@protonmail.com for further support.");
+                    System.exit(1);
+                }
+            } catch (IOException ioe){
+                ioe.printStackTrace();
+                new AlertView().showErrorAlertWait("Unable to verify product key. Please check your internet connection.\n" + ioe.getMessage());
+                System.exit(1);
+            }
+        } else {
+            // This is the first run, have user enter key and check it with the server
+            Stage stage = new Stage();
+            stage.setMinHeight(300);
+            stage.setMinWidth(300);
+            stage.initStyle(StageStyle.DECORATED);
+            stage.setScene(new Scene(new FirstRunView().getFirstRunView(stage), 300, 225));
+            stage.setOnCloseRequest(a -> System.exit(1)); // If user exits this window, close everything and dont write it
+            FirstRunView.getSend().setOnAction(event -> {
+                String key = FirstRunView.getTextField().getText().trim();
+                try {
+                    String st = new Scanner(new URL("http://45.55.208.158:8001/check-id/" + key).openStream(), "UTF-8").useDelimiter("\\A").next();
+                    if (st.equals("{\"status\":\"false\"}")) {
+                        new AlertView().showErrorAlertWait("This software was either pirated or downloaded illegally. Please buy it at https://selly.gg/p/b7e477ee.");
+                        System.exit(1);
+                    } else if (st.equals("{\"status\":\"true\"}")){
+                        System.out.println("Activating key " + key);
+                        new URL("http://45.55.208.158/activate-id/" + key).openStream().close(); // No return body
+                        System.out.println("Key activated");
+                        // Write key to file
+                        PrintWriter pr = new PrintWriter(firstStartUp);
+                        pr.write(key);
+                        pr.close();
+                        System.out.println("Key written to file");
+                    }
+                } catch (IOException ioe){
+                    ioe.printStackTrace();
+                    new AlertView().showErrorAlertWait("\"Unable to verify ID. Please check your internet connection.\"\n" + ioe.getMessage());
+                    System.exit(1);
+                }
+                stage.close();
+            });
+            stage.showAndWait();
+        }
 
         /* Ensure that the necessary files exist */
         new PseudoBase().createKumoData();
